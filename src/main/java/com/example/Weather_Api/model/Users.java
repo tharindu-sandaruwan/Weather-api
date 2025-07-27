d04fa8f5771ea98ebac4a4022de4cab8107f5753
Tharindu Sandaruwan <tharindusandaruwan011@gmail.com>
2025-07-27 15:29:22 +0530

    finished jwt validation

--- src/main/java/com/example/Weather_Api/model/Users.java
+++ src/main/java/com/example/Weather_Api/model/Users.java
@@ -1,12 +1,15 @@
 package com.example.Weather_Api.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Users {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String username;
     private String password;
